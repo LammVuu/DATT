369ecbe535dcaf683c896137ffba37b5b1033dce
mtiendat <73454933+mtiendat@users.noreply.github.com>
2021-04-16 20:41:54 +0700

    Cắt layout Admin

---
+++ LDMobile/resources/views/admin/footer/foot.blade.php
@@ -0,0 +1,17 @@
+<script type="text/javascript" src="../admin/js/jquery/jquery.min.js "></script>
+    <script type="text/javascript" src="../admin/js/jquery-ui/jquery-ui.min.js "></script>
+    <script type="text/javascript" src="../admin/js/popper.js/popper.min.js"></script>
+    <script type="text/javascript" src="../admin/js/bootstrap/js/bootstrap.min.js "></script>
+    <!-- waves js -->
+    <script src="../admin/pages/waves/js/waves.min.js"></script>
+    <!-- jquery slimscroll js -->
+    <script type="text/javascript" src="../admin/js/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- slimscroll js -->
+    <script src="../admin/js/jquery.mCustomScrollbar.concat.min.js "></script>
+
+    <!-- menu js -->
+    <script src="../admin/js/pcoded.min.js"></script>
+    <script src="../admin/js/vertical/vertical-layout.min.js "></script>
+
+    <script type="text/javascript" src="../admin/js/script.js "></script>
--- LDMobile/routes/web.php
+++ LDMobile/routes/web.php
@@ -22,6 +22,22 @@ Route::group(['prefix' => '', 'namespace' => 'user'], function() {
     Route::get("account","UserController@Account")->name("user/account");
     Route::get("cart","CartController@MyCart")->name("user/MyCart");
 });
+Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function() {
+    Route::resource('dashboard', DashboardController::class);
+    Route::resource('hinhanh', HinhAnhController::class);
+    Route::resource('banner', BannerController::class);
+    Route::resource('slideshow', SlideshowController::class);
+    Route::resource('mausanpham', MauSanPhamController::class);
+    Route::resource('sanpham', SanPhamController::class);
+    Route::resource('nhacungcap', NhaCungCapController::class);
+    Route::resource('danhgia', DanhGiaController::class);
+    Route::resource('khuyenmai', KhuyenMaiController::class);
+    Route::resource('donhang', DonHangController::class);
+    Route::resource('baohanh', BaoHanhController::class);
+    Route::resource('taikhoan', TaiKhoanController::class);
+});
+
+
 // Route::get('/', function () {
 //     return view('user/content/index');
    
