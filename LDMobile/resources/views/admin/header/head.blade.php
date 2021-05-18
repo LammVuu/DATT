803bdf4201a4d8bc752a16985edbead153e1d4c9
mtiendat <73454933+mtiendat@users.noreply.github.com>
2021-05-18 16:52:45 +0700

    update admin and images and json of xiaomi note 9 pro

--- LDMobile/resources/views/admin/header/head.blade.php
+++ LDMobile/resources/views/admin/header/head.blade.php
@@ -1,28 +1,30 @@
 <head>
-    <title>../admin LDMobile</title>
+    <title>Admin LDMobile</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <meta name="keywords" content="bootstrap, bootstrap ../admin template, ../admin theme, ../admin dashboard, dashboard template, ../admin template, responsive" />
     <meta name="author" content="Codedthemes" />
+    <base href="{{asset("admin/")}}/">
     <!-- Favicon icon -->
-    <link rel="icon" href="../admin/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
     <!-- waves.css -->
-    <link rel="stylesheet" href="../admin/pages/waves/css/waves.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="pages/waves/css/waves.min.css" type="text/css" media="all">
     <!-- Required Fremwork -->
-    <link rel="stylesheet" type="text/css" href="../admin/css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
     <!-- waves.css -->
-    <link rel="stylesheet" href="../admin/pages/waves/css/waves.min.css" type="text/css" media="all">
+    <link rel="stylesheet" href="pages/waves/css/waves.min.css" type="text/css" media="all">
     <!-- themify icon -->
-    <link rel="stylesheet" type="text/css" href="../admin/icon/themify-icons/themify-icons.css">
+    <link rel="stylesheet" type="text/css" href="icon/themify-icons/themify-icons.css">
     <!-- font-awesome-n -->
-    <link rel="stylesheet" type="text/css" href="../admin/css/font-awesome-n.min.css">
-    <link rel="stylesheet" type="text/css" href="../admin/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome-n.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
     <!-- scrollbar.css -->
-    <link rel="stylesheet" type="text/css" href="../admin/css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery.mCustomScrollbar.css">
     <!-- Style.css -->
-    <link rel="stylesheet" type="text/css" href="../admin/css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
 </head>
