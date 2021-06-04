c8d56cd14dbc9f3937e0dc33110d59d8b57936c5
mtiendat <73454933+mtiendat@users.noreply.github.com>
2021-06-04 15:26:28 +0700

    add images slideshow

--- LDMobile/app/Models/TAIKHOAN.php
+++ LDMobile/app/Models/TAIKHOAN.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TAIKHOAN extends Models
+class TAIKHOAN extends Model
 {
     use HasFactory;
 
