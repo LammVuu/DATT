9fa11843bae4c90ff555fd5de5f51b47ded4520f
mtiendat <73454933+mtiendat@users.noreply.github.com>
2021-10-14 09:15:11 +0700

    Update PushnotificationController.php

--- LDMobile/app/Http/Controllers/PushnotificationController.php
+++ LDMobile/app/Http/Controllers/PushnotificationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Traits\PushNotificationTrait;
 
-class PushnotificationController extends Controller
+class PushNotificationController extends Controller
 {
 
     use PushNotificationTrait;
